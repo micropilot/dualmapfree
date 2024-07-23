95b9c009879aff4b33319136cb7382b1cf569127
Hitesh Jain <76439813+HITESH2002-JAIN@users.noreply.github.com>
2024-07-23 17:11:50 +0530

    Update clip_batch.py

--- 4M_dataset_dev/clip/clip_batch.py
+++ 4M_dataset_dev/clip/clip_batch.py
@@ -12,17 +12,19 @@ model, preprocess = clip.load("ViT-B/16", device=device)
 model.eval()
 image_files = glob.glob("/mnt/SSD1/Niantic/data/train/**/**/*.jpg")
 batch_size = 2000
-
 common_txt_path = '/mnt/SSD1/Niantic/clip_feat/batch_paths.txt'
-with open(common_txt_path, 'w') as common_file:
-    common_file.write("")
+if os.path.exists(common_txt_path):
+    pass
+else:
+    with open(common_txt_path, 'w') as common_file:
+        common_file.write("")
 
-for index in tqdm(range(0, len(image_files), batch_size), desc="Processing batches"):
+for i,index in enumerate(tqdm(range(0, len(image_files), batch_size), desc="Processing batches")):
     batch_start = index
     batch_end = min(index + batch_size, len(image_files))
     batch_path = image_files[batch_start:batch_end]
 
-    if os.path.exists(f'/mnt/SSD1/Niantic/clip_feat/batch_{index}.pt'):
+    if os.path.exists(f'/mnt/SSD1/Niantic/clip_feat/batch_{i}.pt'):
         continue
     
     batch_images = [preprocess(Image.open(image_path)).unsqueeze(0) for image_path in batch_path]
@@ -33,7 +35,7 @@ for index in tqdm(range(0, len(image_files), batch_size), desc="Processing batch
 
     image_features_cpu = image_features.cpu()
 
-    torch.save(image_features_cpu, f'/mnt/SSD1/Niantic/clip_feat/batch_{index}.pt')
+    torch.save(image_features_cpu, f'/mnt/SSD1/Niantic/clip_feat/batch_{i}.pt')
     
     with open(common_txt_path, 'a') as common_file:
-        common_file.write(f"batch_{index}: {batch_path}\n")
+        common_file.write(f"batch_{i}: {batch_path}\n")
