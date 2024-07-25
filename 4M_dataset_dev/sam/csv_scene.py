4ecdb0e2431aa09b4075a760049fe593ea34161d
Hitesh Jain <76439813+HITESH2002-JAIN@users.noreply.github.com>
2024-07-25 20:01:54 +0530

    Create csv_scene.py

---
+++ 4M_dataset_dev/sam/csv_scene.py
@@ -0,0 +1,26 @@
+import glob
+import csv
+import os
+
+train_scenes = glob.glob("/mnt/SSD1/Niantic/data/train/**")
+val_scenes = glob.glob("/mnt/SSD1/Niantic/data/train/val/*")
+scenes = train_scenes + val_scenes
+num_csv = 8
+scenes_per_csv = len(scenes) // num_csv
+
+output_dir = "csv"
+os.makedirs(output_dir, exist_ok=True)
+
+for i in range(num_csv):
+    start_index = i * scenes_per_csv
+    if i == num_csv - 1:  
+        end_index = len(scenes)
+    else:
+        end_index = (i + 1) * scenes_per_csv
+    
+    csv_filename = os.path.join(output_dir, f"scenes_part_{i}.csv")
+    
+    with open(csv_filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for scene in scenes[start_index:end_index]:
+            writer.writerow([scene])
